1dcd8609909d37fd8a611a7c11fec93e0c1b7cc5
Samuel de Morais <ssamueldemorais@gmail.com>
2023-10-14 18:45:32 -0300

    mapeamento do banco

--- src/main/java/com/colegiado/sistemacolegiado/models/Voto.java
+++ src/main/java/com/colegiado/sistemacolegiado/models/Voto.java
@@ -1,22 +1,40 @@
 package com.colegiado.sistemacolegiado.models;
 
 import com.colegiado.sistemacolegiado.models.enums.TipoVoto;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-
+@Entity
 public class Voto {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
+
+    @EmbeddedId
+    private VotoId id;
+
+    @ManyToOne
+    @MapsId("id_professor")
+    @JoinColumn(name="id_professor")
+    private Professor professor;
+    @ManyToOne
+    @MapsId("id_processo")
+    @JoinColumn(name= "id_processo")
+    private Processo processo;
     private TipoVoto voto;
+}
+
+@Embeddable
+class VotoId implements Serializable {
+    @Column(name = "id_professor")
+    private int idProfessor;
+    @Column(name ="id_processo")
+    private int idProcesso;
+
 }
